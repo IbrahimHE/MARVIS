4981dbeb9398dd32ed25f1f4401167322c000fe9
IbrahimHassan <44149723+IbrahimHE@users.noreply.github.com>
2022-03-05 14:29:18 +0200

    new

--- R/startmarvis.R
+++ R/startmarvis.R
@@ -1,16 +1,16 @@
-#' @title Launch MARVIS interactive User Interface
-#' @return Nothing
-#' @description startMARVIS starts loads the web browser an interactive user interface built using R shiny.
-#' @details The purpose of building the interactive user interface is to provide an easy for people who are learning how to do their first meta-analysis and/or are not comfortable with the R command line system. Includes example data for testing out a meta-analysis.
-#' @keywords MARVIS
-#' @examples
-#' \dontrun{
-#' startMARVIS()
-#' }
-#' @importFrom colourpicker colourInput
-#' @import fpc ggplot2 ggrepel gridExtra htmlTable kableExtra meta
-#' metafor poibin readxl shiny shinyFeedback shinymanager stringr tools
-#' @export startMARVIS
+# @title Launch MARVIS interactive User Interface
+# @return Nothing
+# @description startMARVIS starts loads the web browser an interactive user interface built using R shiny.
+# @details The purpose of building the interactive user interface is to provide an easy for people who are learning how to do their first meta-analysis and/or are not comfortable with the R command line system. Includes example data for testing out a meta-analysis.
+# @keywords MARVIS
+# @examples
+# \dontrun{
+# startMARVIS()
+# }
+# @importFrom colourpicker colourInput
+# @import fpc ggplot2 ggrepel gridExtra htmlTable kableExtra meta
+# metafor poibin readxl shiny shinyFeedback shinymanager stringr tools
+# @export startMARVIS
 # startMARVIS <- function() {
 #   shiny::runApp(appDir = system.file("shiny", package="MARVIS"))
 # }
